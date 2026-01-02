a23bf1bb8370407a389eca11db21a46d7c5c904f
Simon Siena <ssiena@uwaterloo.ca>
2026-01-02 13:08:05 -0500

    add nvim-treesitter-context

--- init.lua
+++ init.lua
@@ -62,6 +62,7 @@ require("lazy").setup({
 	require("plugins.bufferline"),
 	require("plugins.lualine"),
 	require("plugins.treesitter"),
+	require("plugins.treesitter-context"),
 	require("plugins.telescope"),
 	require("plugins.lsp"),
 	require("plugins.autocompletion"),
